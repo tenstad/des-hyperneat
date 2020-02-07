5575d9afc3075a545eaee6888a359c4ac9c0e1a6
Amund Tenstad <github@amund.io>
2020-02-07 09:38:00 +0100

    Profile execution time

---
+++ flame_highlight.py
@@ -0,0 +1,22 @@
+NAME = 'des_hyperneat'
+
+
+with open('flamegraph.svg') as f:
+	content = f.read()
+i = 0
+
+while 1:
+	i = content.find('<g><title>', i)
+	if i < 0:
+		break
+	i += 10
+	j = content.find('</title>', i)
+	name = content[i:j]
+	if not NAME in name or '{{closure}}' in name:
+		k = content.find('rgb(', i)
+		l = content.find(')', k) + 1
+		content = content[:k] + 'rgba(0, 0, 0, 0.2)' + content[l:]
+	i = j
+
+with open('flamegraph_highlight.svg', 'w') as f:
+	f.write(content)
