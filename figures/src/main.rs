39a894f615bab08b2fa04abccecb7a113a3e8acf
Amund Tenstad <github@amund.io>
2020-04-16 18:08:03 +0200

    Create local and jumper connection figures

--- figures/src/main.rs
+++ figures/src/main.rs
@@ -1,7 +1,11 @@
-mod blueprint;
 mod assembled;
+mod blueprint;
+mod local_connections;
+mod jumper_connections;
 
 fn main() {
     blueprint::create().save("outputs/blueprint.tex");
     assembled::create().save("outputs/assembled.tex");
+    local_connections::create().save("outputs/local_connections.tex");
+    jumper_connections::create().save("outputs/jumper_connections.tex");
 }
