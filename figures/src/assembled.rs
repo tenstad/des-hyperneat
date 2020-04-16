c1d69caf77be4e66f19ac30f20e1ff71d06cf0b5
Amund Tenstad <github@amund.io>
2020-04-16 17:15:18 +0200

    Set depth to 0 in I/O figure substrates

--- figures/src/assembled.rs
+++ figures/src/assembled.rs
@@ -5,8 +5,8 @@ pub fn create() -> figure::Figure {
 
     let mut substrate = fig.substrate_builder(&|s| s.size(2.0).cells(2).axis_arrow_offset(0.15));
     let s = substrate(&|s| s);
-    let w = substrate(&|s| s.x(-4.2).y(5.0));
-    let e = substrate(&|s| s.x(4.2).y(5.0));
+    let w = substrate(&|s| s.x(-3.2).y(5.0));
+    let e = substrate(&|s| s.x(3.2).y(5.0));
     let n = substrate(&|s| s.y(10.0));
     drop(substrate);
 
@@ -18,55 +18,37 @@ pub fn create() -> figure::Figure {
     drop(node);
 
     let mut node = fig.node_builder(&|n| n);
-    let s00 = node(&|n| n.x(0.85).y(1.75));
-    let s01 = node(&|n| n.x(1.8).y(1.4));
-    let s02 = node(&|n| n.x(0.4).y(0.2));
+    let s10 = node(&|n| n.x(-0.3).y(3.55));
+    let s12 = node(&|n| n.x(-1.2).y(4.0));
+    let s13 = node(&|n| n.x(0.1).y(2.95));
+    let s14 = node(&|n| n.x(0.1).y(4.15));
 
-    let s10 = node(&|n| n.x(-0.8).y(3.55));
-    let s12 = node(&|n| n.x(-1.7).y(4.0));
-    let s13 = node(&|n| n.x(-0.4).y(2.95));
-    let s14 = node(&|n| n.x(-0.4).y(4.15));
-
-    let s20 = node(&|n| n.x(2.4).y(3.2));
-    let s22 = node(&|n| n.x(3.9).y(2.75));
-    let s23 = node(&|n| n.x(3.2).y(4.15));
-
-    let s30 = node(&|n| n.x(0.4).y(5.4));
-    let s31 = node(&|n| n.x(0.5).y(6.8));
-    let s32 = node(&|n| n.x(1.5).y(6.6));
+    let s20 = node(&|n| n.x(1.9).y(3.2));
+    let s22 = node(&|n| n.x(3.6).y(2.75));
+    let s23 = node(&|n| n.x(2.7).y(4.15));
     drop(node);
 
     let mut edge = fig.edge_builder(&|e| e.width(0.2));
-    edge(&i0, &s00, &|e| e);
-    edge(&i0, &s02, &|e| e);
-    edge(&i1, &s00, &|e| e);
-    edge(&i1, &s01, &|e| e);
+    edge(&i1, &s13, &|e| e);
+    edge(&i0, &s12, &|e| e);
 
-    edge(&s00, &s13, &|e| e);
-    edge(&s02, &s12, &|e| e);
-
-    edge(&s12, &s31, &|e| e);
+    edge(&s12, &o, &|e| e);
     edge(&s12, &s10, &|e| e);
     edge(&s13, &s10, &|e| e);
     edge(&s10, &s14, &|e| e);
 
-    edge(&s00, &s20, &|e| e);
-    edge(&s01, &s20, &|e| e);    
+    edge(&i1, &s20, &|e| e);
+    edge(&i2, &s20, &|e| e);    
     edge(&i2, &s22, &|e| e);
 
     edge(&s20, &s23, &|e| e);
     edge(&s22, &s23, &|e| e);
     edge(&s20, &s10, &|e| e);
 
-    edge(&s14, &s30, &|e| e);
+    edge(&s14, &o, &|e| e);
 
     edge(&s23, &o, &|e| e);
-    edge(&s20, &s30, &|e| e);
-
-    edge(&s30, &o, &|e| e);
-    edge(&s31, &o, &|e| e);
-    edge(&s31, &s32, &|e| e);
-    edge(&s32, &o, &|e| e);
+    edge(&s20, &o, &|e| e);
     drop(edge);
 
     fig
--- figures/src/blueprint.rs
+++ figures/src/blueprint.rs
@@ -3,10 +3,10 @@ use figure;
 pub fn create() -> figure::Figure {
     let mut fig = figure::Figure::new();
 
-    let mut node = fig.node_builder(&|n| n.size(12.0).edge_offset(2.0).text("d: 1"));
+    let mut node = fig.node_builder(&|n| n.size(12.0).edge_offset(2.0).text("d: 0"));
     let s = node(&|n| n);
     let w = node(&|n| n.x(-2_f64.sqrt()).y(2.0).text("d: 2"));
-    let e = node(&|n| n.x(2_f64.sqrt()).y(2.0));
+    let e = node(&|n| n.x(2_f64.sqrt()).y(2.0).text("d: 1"));
     let n = node(&|n| n.y(4.0));
     drop(node);
 
