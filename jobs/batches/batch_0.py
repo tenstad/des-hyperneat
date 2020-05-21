c667f3bf7dc206c6cf6eee40f6f164fdc79d2b4b
Amund Tenstad <github@amund.io>
2020-05-21 16:30:38 +0200

    Add non-adaptive to batch 0

--- jobs/batches/batch_0.py
+++ jobs/batches/batch_0.py
@@ -16,18 +16,30 @@ def run():
         'DROPOFF_AGE': [20, 100],
     }
 
+    param_grid_non_adaptive = {
+        'METHOD': ['NEAT', 'CPPN', 'HyperNEAT'],
+        'DATASET': ['datasets/generated/iris',
+                    'datasets/generated/wine'],
+        'ADAPTIVE_SPECIATION_THRESHOLD': [False],
+        'INITIAL_MUTATIONS': [100, 250, 500],
+        'DROPOFF_AGE': [20, 100],
+    }
+
     static_params = {
         'POPULATION_SIZE': 200,
         'ITERATIONS': 300,
         'LOG_INTERVAL': 15,
     }
 
-    for params in ParameterGrid(param_grid):
-        if params['METHOD'] in ('NEAT', 'HyperNEAT'):
-            params['OUTPUT_ACTIVATION'] = 'Softmax'
-        elif params['METHOD'] == 'CPPN':
-            params['OUTPUT_ACTIVATIONS'] = 'Softmax'
+    def run_grid(grid):
+        for params in ParameterGrid(grid):
+            if params['METHOD'] in ('NEAT', 'HyperNEAT'):
+                params['OUTPUT_ACTIVATION'] = 'Softmax'
+            elif params['METHOD'] == 'CPPN':
+                params['OUTPUT_ACTIVATIONS'] = 'Softmax'
+            name = str(params)
+            params.update(static_params)
+            sheduler.create_job(BATCH, name, REPEATS, params)
 
-        name = str(params)
-        params.update(static_params)
-        sheduler.create_job(BATCH, name, REPEATS, params)
+    run_grid(param_grid)
+    run_grid(param_grid_non_adaptive)
